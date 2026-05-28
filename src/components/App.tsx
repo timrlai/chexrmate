3a306906688e8704fe816e713cf3bc7b23276500
Tim Lai <timlaicodes@gmail.com>
2026-05-27 20:39:46 -0400

    Updated App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,49 +1,50 @@
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import { XR, createXRStore } from "@react-three/xr";
-import { Box3 } from "three";
-import Intersectable from "./Intersectable";
-import Draggable from "./Draggable";
-import Model from "./Model";
+import Board from "./Board";
+import Opponent from "./Opponent";
+import Locomotion from "./Locomotion";
 
 const store = createXRStore();
 
 export default function App() {
-  const [box, setBox] = useState<Box3 | null>(null);
-  const [red, setRed] = useState(false);
-
   return (
     <main>
+      <header>
+        <h1>CheXRmate</h1>
+      </header>
       <nav id="xr-button-container">
-        {/* <button onClick={() => store.enterAR()}>Enter AR</button> */}
         <button
-          onClick={() => store.enterVR()}
+          onClick={() => store.enterAR()}
           className="special-gothic-condensed-one-regular"
         >
-          Enter VR
+          Enter AR
         </button>
       </nav>
-      <Canvas>
+      <Canvas shadows>
         <XR store={store}>
           <Suspense fallback={null}>
-            <Intersectable
-              position={[-2, 1, -1]}
-              box={box}
-              onIntersect={() => setRed(true)}
-            >
-              <mesh>
-                <boxGeometry />
-                <meshBasicMaterial color={red ? "red" : "blue"} />
-              </mesh>
-            </Intersectable>
-            <Draggable position={[2, 1, -1]} onDragged={(box) => setBox(box)}>
-              <mesh>
-                <boxGeometry />
-                <meshBasicMaterial color="red" />
-              </mesh>
-            </Draggable>
-            <Model />
+            <Opponent />
+
+            <Board />
+
+            <ambientLight
+              position={[0, 10, -12]}
+              color="pink"
+              intensity={0.6}
+              castShadow
+            />
+            <directionalLight
+              position={[3, 8, -10]}
+              color="lightyellow"
+              intensity={0.7}
+              castShadow
+            />
+
+            <Environment preset="park" environmentIntensity={0.5} />
+
+            <Locomotion />
 
             <OrbitControls />
           </Suspense>
