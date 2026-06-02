df9a60aa7d4dbb806070440fc0bc9e5cb8d3d45c
Tim Lai <timlaicodes@gmail.com>
2026-06-02 16:56:40 -0400

    Added XRButton to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,6 +9,7 @@ import Opponent from "./Opponent";
 import Locomotion from "./Locomotion";
 import XRScene from "./XRScene";
 import PreloadFont from "./PreloadFont";
+import XRButton from "./XRButton";
 
 type ComponentFallbackProps = {
   componentName: string;
@@ -42,27 +43,16 @@ export default function App() {
     undefined,
   );
 
-  const onEnterXr = () => {
-    if (!xrStore) return;
-    xrStore.enterAR().then((session) => {
-      console.log("session:", session);
-      if (!xrSession && session) setXrSession(session);
-    });
-  };
-
   return (
     <main>
       <header>
         <h1>CheXRmate</h1>
       </header>
-      <nav id="xr-button-container">
-        <button
-          onClick={onEnterXr}
-          className="special-gothic-condensed-one-regular"
-        >
-          Enter AR
-        </button>
-      </nav>
+      <XRButton
+        store={xrStore}
+        existingSession={xrSession}
+        setSession={setXrSession}
+      />
       <Canvas shadows>
         <XRScene existingStore={xrStore} setXrStore={setXrStore}>
           <>
